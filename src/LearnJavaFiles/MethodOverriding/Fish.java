dd9e1316161de452844c5e65ef4403b4dddefe49
Abhay909 <87679962+Abhay909@users.noreply.github.com>
2025-01-24 20:41:31 -0800

    Grammar errors

--- src/LearnJavaFiles/MethodOverriding/Fish.java
+++ src/LearnJavaFiles/MethodOverriding/Fish.java
@@ -2,7 +2,7 @@ package LearnJavaFiles.MethodOverriding;
 
 public class Fish extends Animal{
 
-    // Good practice so you and other devoilopers knwo tht im overriding the orignal animal command because fish cant run
+    // Good practice, so you and other developers know tht im overriding the original animal command because fish cant run
     @Override
     void move() {
         System.out.println("This animal is swimming");
